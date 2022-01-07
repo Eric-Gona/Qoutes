219f066b81e4c894c6ab27f7b7f80f47404a0774
Eric-Gona <eric.gona254@gmail.com>
2022-01-07 16:09:21 +0300

    makes changes to app.modules.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './Qoute';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { DatePipePipe } from './date-pipe.pipes.spects';
+import { FormComponent } from './Form';
+import { HighlightQouteDirective } from './highlight-qoute.directive';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-    QouteComponent,
-    Quote-detailsComponent,
-    FormsComponent,
-   
+    AppComponent,
+    QuotesComponent,
+    QuoteDetailsComponent,
+    DatePipePipe,
+    FormComponent,
+    HighlightQouteDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    SweetAlert2Module.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
